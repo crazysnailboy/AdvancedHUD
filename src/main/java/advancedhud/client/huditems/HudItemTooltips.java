a2c1b4bda96bb5bcb1a59c0f11e83413d4220ab3
Chris Porter <github@crazysnailboy.net>
2017-09-09 12:37:14 +0100

    fixed item tooltips not fading out

--- src/main/java/advancedhud/client/huditems/HudItemTooltips.java
+++ src/main/java/advancedhud/client/huditems/HudItemTooltips.java
@@ -1,24 +1,22 @@
 package advancedhud.client.huditems;
 
+import org.lwjgl.opengl.GL11;
 import advancedhud.api.Alignment;
 import advancedhud.api.HUDRegistry;
 import advancedhud.api.HudItem;
 import advancedhud.client.ui.GuiAdvancedHUDConfiguration;
 import advancedhud.client.ui.GuiScreenHudItem;
 import advancedhud.client.ui.GuiScreenReposition;
-import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumChatFormatting;
 
 public class HudItemTooltips extends HudItem {
 
-    private String itemName;
-    private String itemRarityColorCode;
-    private int stringColor;
-    private float alpha;
-    private int updateCounter;
-    private boolean resetFadeTimer;
+    private int remainingHighlightTicks;
+    private ItemStack highlightingItemStack;
 
     @Override
     public String getName() {
@@ -63,50 +61,62 @@ public class HudItemTooltips extends HudItem {
     @Override
     public void render(float partialTicks) {
 
+        mc.mcProfiler.startSection("toolHighlight");
+
+        boolean renderTooltip = false;
+        String tooltipText = null;
+        int opacity = 255;
+
         if (this.mc.currentScreen instanceof GuiAdvancedHUDConfiguration || this.mc.currentScreen instanceof GuiScreenReposition) {
-            this.itemName = this.getButtonLabel();
+            renderTooltip = true;
+            tooltipText = this.getButtonLabel();
+        } else if (this.mc.gameSettings.heldItemTooltips) {
+            if (this.remainingHighlightTicks > 0 && this.highlightingItemStack != null) {
+                renderTooltip = true;
+                tooltipText = this.highlightingItemStack.getDisplayName();
+                if (this.highlightingItemStack.hasDisplayName()) tooltipText = EnumChatFormatting.ITALIC + tooltipText;
+                opacity = (int)((float)this.remainingHighlightTicks * 256.0F / 10.0F);
+                if (opacity > 255) opacity = 255;
+            }
         }
 
-        if (this.itemName != null && !this.itemName.isEmpty()) {
-            FontRenderer fontrenderer = this.mc.fontRendererObj;
+        if (renderTooltip && opacity > 0) {
             int posX;
             if (Alignment.isLeft(this.alignment)) {
                 posX = this.posX;
             } else if (Alignment.isHorizontalCenter(this.alignment)) {
-                posX = this.posX + (this.getWidth() - fontrenderer.getStringWidth(this.itemName)) / 2;
+                posX = this.posX + (this.getWidth() - this.mc.fontRendererObj.getStringWidth(tooltipText)) / 2;
             } else {
-                posX = this.posX + this.getWidth() - fontrenderer.getStringWidth(this.itemName);
+                posX = this.posX + this.getWidth() - this.mc.fontRendererObj.getStringWidth(tooltipText);
             }
 
-            fontrenderer.drawStringWithShadow(this.itemRarityColorCode + this.itemName, posX, this.posY, this.stringColor);
+            GlStateManager.pushMatrix();
+            GlStateManager.enableBlend();
+            GlStateManager.tryBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, 1, 0);
+
+            this.mc.fontRendererObj.drawStringWithShadow(tooltipText, posX, this.posY, 0xFFFFFF | (opacity << 24));
+
+            GlStateManager.disableBlend();
+            GlStateManager.popMatrix();
         }
+
+        mc.mcProfiler.endSection();
     }
 
     @Override
     public void tick() {
         if (this.mc.thePlayer != null) {
-            ItemStack currentItem = this.mc.thePlayer.inventory.getCurrentItem();
-            String currentName = currentItem == null ? "" : currentItem.getDisplayName();
-
-            this.resetFadeTimer = !currentName.equals(this.itemName);
-            this.itemName = currentName;
-
-            if (currentItem != null) {
-                this.itemRarityColorCode = currentItem.getRarity().rarityColor.toString();
-                this.stringColor = 0xFFFFFF;
+            ItemStack itemstack = this.mc.thePlayer.inventory.getCurrentItem();
+            if (itemstack == null) {
+                this.remainingHighlightTicks = 0;
+            } else if (this.highlightingItemStack != null && itemstack.getItem() == this.highlightingItemStack.getItem() && ItemStack.areItemStackTagsEqual(itemstack, this.highlightingItemStack) && (itemstack.isItemStackDamageable() || itemstack.getMetadata() == this.highlightingItemStack.getMetadata())) {
+                if (this.remainingHighlightTicks > 0) {
+                    --this.remainingHighlightTicks;
+                }
+            } else {
+                this.remainingHighlightTicks = 40;
             }
-        }
-
-        if (this.resetFadeTimer) {
-            this.alpha = 1.0F;
-
-            int fadeSpeed = 8 * 20;
-
-            this.updateCounter = HUDRegistry.updateCounter + fadeSpeed;
-            this.resetFadeTimer = false;
-        } else {
-            this.alpha = (this.updateCounter - HUDRegistry.updateCounter) / 20.0F;
-            this.alpha = Math.min(Math.max(this.alpha, 0.0F), 1.0F);
+            this.highlightingItemStack = itemstack;
         }
     }
 
