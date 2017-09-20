da72fb95103cd70145da84e1ef43d6c66caa2727
Chris Porter <github@crazysnailboy.net>
2017-09-20 14:44:33 +0100

    made health mount rotate

--- src/main/java/advancedhud/client/huditems/HudItemHealthMount.java
+++ src/main/java/advancedhud/client/huditems/HudItemHealthMount.java
@@ -24,27 +24,27 @@ public class HudItemHealthMount extends HudItem {
 
     @Override
     public Alignment getDefaultAlignment() {
-        return Alignment.BOTTOMCENTER;
+        return (!this.rotated ? Alignment.BOTTOMCENTER : Alignment.CENTERRIGHT);
     }
 
     @Override
     public int getDefaultPosX() {
-        return HUDRegistry.screenWidth / 2 + 10;
+        return (!this.rotated ? HUDRegistry.screenWidth / 2 + 10 : HUDRegistry.screenWidth - 39);
     }
 
     @Override
     public int getDefaultPosY() {
-        return HUDRegistry.screenHeight - 39;
+        return (!this.rotated ? HUDRegistry.screenHeight - 39 : HUDRegistry.screenHeight / 2 + 10);
     }
 
     @Override
     public int getWidth() {
-        return 81;
+        return (!this.rotated ? 81 : 9);
     }
 
     @Override
     public int getHeight() {
-        return 9;
+        return (!this.rotated ? 9 : 81);
     }
 
     @Override
@@ -54,44 +54,36 @@ public class HudItemHealthMount extends HudItem {
         if (ridingEntity == null && this.configMode()) ridingEntity = new EntityHorse(this.mc.theWorld);
         if (!(ridingEntity instanceof EntityLivingBase)) return;
 
+        EntityLivingBase mount = (EntityLivingBase)ridingEntity;
+        int health = (int)Math.ceil(mount.getHealth());
+        double healthMax = mount.getEntityAttribute(SharedMonsterAttributes.maxHealth).getAttributeValue();
+        int hearts = (int)Math.ceil(((float)healthMax + 0.5F) / 2F);
+        if (hearts > 30) hearts = 30;
+
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
         this.mc.renderEngine.bindTexture(Gui.icons);
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 
-        int left_align = this.posX + 81;
-        int right_height = 1;
-
-        EntityLivingBase mount = (EntityLivingBase)ridingEntity;
-        int health = (int)Math.ceil(mount.getHealth());
-        double healthMax = mount.getEntityAttribute(SharedMonsterAttributes.maxHealth).getAttributeValue();
-        int hearts = (int)Math.ceil(((float)healthMax + 0.5F) / 2F);
-
-        if (hearts > 30) hearts = 30;
-
-        final int MARGIN = 52;
-        final int BACKGROUND = MARGIN;
-        final int HALF = MARGIN + 45;
-        final int FULL = MARGIN + 36;
+        int align = (!this.rotated ? this.posX + this.getWidth() : this.posY + this.getHeight());
+        int offset = 0;
 
         for (int heart = 0; hearts > 0; heart += 20) {
 
-            int top = this.posY + 1 - right_height;
-            int rowCount = Math.min(hearts, 10);
-            hearts -= rowCount;
+            int top = (!this.rotated ? this.posY + offset : 0);
+            int left = (!this.rotated ? 0 : this.posX + offset);
 
-            for (int i = 0; i < rowCount; ++i) {
-                int x = left_align - i * 8 - 9;
-                this.drawTexturedModalRect(x, top, BACKGROUND, 9, 9, 9);
+            int count = Math.min(hearts, 10);
+            hearts -= count;
 
-                if (i * 2 + 1 + heart < health) {
-                    this.drawTexturedModalRect(x, top, FULL, 9, 9, 9);
-                } else if (i * 2 + 1 + heart == health) {
-                    this.drawTexturedModalRect(x, top, HALF, 9, 9, 9);
-                }
+            for (int i = 0; i < count; ++i) {
+                int x = (!this.rotated ? align - i * 8 - 9 : left);
+                int y = (!this.rotated ? top : align - i * 8 - 9);
 
-                right_height = i + 1;
+                this.drawTexturedModalRect(x, y, 52, 9, 9, 9);
+                this.drawTexturedModalRect(x, y, ((i * 2 + 1 + heart < health) ? 88 : 97), 9, 9, 9);
             }
+            offset += 10;
         }
 
         GL11.glDisable(GL11.GL_BLEND);
