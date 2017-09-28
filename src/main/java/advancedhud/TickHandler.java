4a8d05c0c3f7f1d1001c6864785a91fd214d6443
Chris Porter <github@crazysnailboy.net>
2017-09-28 16:46:58 +0100

    Added solid bar rendering style for some items

--- src/main/java/advancedhud/TickHandler.java
+++ src/main/java/advancedhud/TickHandler.java
@@ -1,8 +1,8 @@
 package advancedhud;
 
-import advancedhud.api.HUDRegistry;
 import advancedhud.client.GuiAdvancedHUD;
 import net.minecraft.client.Minecraft;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
 import net.minecraftforge.fml.common.gameevent.TickEvent.RenderTickEvent;
@@ -11,34 +11,32 @@ import net.minecraftforge.fml.common.gameevent.TickEvent.Type;
 public class TickHandler {
 
     private static final Minecraft mc = Minecraft.getMinecraft();
+    private static final TickHandler handler = new TickHandler();
 
-    private boolean ticked = false;
-    private boolean firstload = true;
+    private boolean guiLoaded = false;
+
+
+    public static void register() {
+        MinecraftForge.EVENT_BUS.register(handler);
+    }
+
+    private static void unregister() {
+        MinecraftForge.EVENT_BUS.unregister(handler);
+    }
 
 
     @SubscribeEvent
     public void RenderTickEvent(RenderTickEvent event) {
         if ((event.type == Type.RENDER || event.type == Type.CLIENT) && event.phase == Phase.END) {
-            if (!this.ticked && mc.ingameGUI != null) {
+
+            if (!this.guiLoaded && mc.ingameGUI != null) {
                 mc.ingameGUI = new GuiAdvancedHUD(mc);
-                this.ticked = true;
+                this.guiLoaded = true;
             }
-            if (this.firstload && mc != null) {
-                if (!SaveController.loadConfig("config")) {
-                    HUDRegistry.checkForResize();
-                    HUDRegistry.resetAllDefaults();
-                    SaveController.saveConfig("config");
-                }
-                this.firstload = false;
+
+            if (guiLoaded) {
+                unregister();
             }
-            // TODO Add notification on main menu when an update for advancedhud is available :)
-            // if (mc.currentScreen instanceof GuiMainMenu) {
-            // int mouseX = Mouse.getX() * mc.currentScreen.width / mc.displayWidth;
-            // int mouseY = mc.currentScreen.height - Mouse.getY() * mc.currentScreen.height / mc.displayHeight - 1;
-            // RenderAssist.drawCircle(mouseX, mouseY, 3, 24, 0xFFFFFFFF);
-            // RenderAssist.drawRect(1, 1, 70, 11, 0x608F3EFF);
-            // mc.currentScreen.drawString(mc.fontRenderer, "AdvancedHUD!", 2, 2, 0x48DCE9);
-            // }
         }
     }
 
